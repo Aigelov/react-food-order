e992db69e3e7ac94a66fc2c126811b67f589512e
Galymzhan Aigelov <aigelov@gmail.com>
2022-03-29 01:02:46 +0600

    Eslint Prettier Configure

--- src/App.js
+++
@@ -1,7 +0,0 @@
-export const App = () => {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
-}
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const App = () => (
+  <div>
+    <h2>Let's get started!</h2>
+  </div>
+);
